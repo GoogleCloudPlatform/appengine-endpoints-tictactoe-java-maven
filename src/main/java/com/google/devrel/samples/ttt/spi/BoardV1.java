da24cc8cd115d7a469533db4432bf690513f53dc
Dan Holevoet <danielholevoet@google.com>
2013-10-09 13:26:39 -0700

    Removes id_token references from JS and modifies backend to support it
    Reviewed in https://codereview.appspot.com/14529047/

--- src/main/java/com/google/devrel/samples/ttt/spi/BoardV1.java
+++ src/main/java/com/google/devrel/samples/ttt/spi/BoardV1.java
@@ -29,8 +29,9 @@ import com.google.devrel.samples.ttt.Board;
 @Api(
     name = "tictactoe",
     version = "v1",
-    clientIds = {Ids.WEB_CLIENT_ID, Ids.ANDROID_CLIENT_ID, Ids.IOS_CLIENT_ID},
-    audiences = {Ids.ANDROID_AUDIENCE}
+    scopes = {Constants.EMAIL_SCOPE},
+    clientIds = {Constants.WEB_CLIENT_ID, Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID},
+    audiences = {Constants.ANDROID_AUDIENCE}
 )
 public class BoardV1 {
   public static final char X = 'X';
